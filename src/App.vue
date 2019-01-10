d3029dde4fcd4492bae28cd09c8622473666d020
JIGANL <jigan.l@sfeir.com>
2019-01-10 10:08:04 +0100

    add call axios to api

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,44 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Cats msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
 import Cats from './components/Cats.vue'
+import axios from 'axios';
 
 export default {
-  name: 'app',
-  components: {
-    Cats
-  }
-}
+    name: 'app',
+    components: {
+        Cats
+    },
+    data() {
+        return {
+            cats: []
+        }
+    },
+    mounted() {
+        this.loadList();
+    },
+    methods: {
+        async loadList() {
+            try {
+                axios.defaults.headers.common['x-api-key'] = "ea49e96d-cde8-4501-bf3a-cb71a4cbf868";
+                let response = await axios.get('https://api.thecatapi.com/v1/breeds');
+
+                this.image = response.data[0]; // the response is an Array, so just use the first item as the Image
+                /* eslint no-console: "log" */
+                console.log("-- Image from TheCatAPI.com");
+                console.log("id:", this.image.id);
+                console.log("url:", this.image.url);
+            } catch (err) {
+                console.log(err);
+            }
+        }
+    }
+};
 </script>
 
 <style>
